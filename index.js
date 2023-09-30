a92afd37fc459ef1e4c2a78d81d0148ff74a7ccd
Karim <karim_isaac@hotmail.com>
2023-09-30 14:10:20 +0300

    index is working and add more pics

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-
+let data;
+let result;
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/',(req,res)=>{
-//     res.render('index.ejs')
-//     });
+
+
 app.get("/", async (req, res) => 
 {
     try 
     {
-    const result = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
-    const obj = JSON.stringify(result.data)
-    console.log(typeof result.data)
-    console.log(result.data)
-    res.render("index.ejs",{DrinkType: result});
+      res.render("index.ejs",{DrinkType: data});
+    } 
+    catch (error) 
+    {
+    console.log(error.response.data);
+    }
+});
+
+app.post("/cocktail", async (req, res) => 
+{
+    try 
+    {
+    switch (req.body.choice) {
+      case "wine":
+        result = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.body.choice}`);
+        
+        break;
+         case "gin":
+         result = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.body.choice}`);
+         
+         break;
+         case "whiskey":
+           result = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.body.choice}`);
+           
+           break;
+          case "tequila":
+           result = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.body.choice}`);
+           
+           break;
+           case "vodka":
+           result = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.body.choice}`);
+           
+           break;
+           
+    }
+    data=result.data.drinks[Math.floor(Math.random()*result.data.drinks.length)]
+    res.redirect("/");
+
     } 
     catch (error) 
     {
